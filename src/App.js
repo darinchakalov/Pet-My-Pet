3a07845fab34c5c710598427e1e88b60e7420d29
darinchaklov <darinchakalov@hotmail.com>
2022-05-03 02:47:44 +0300

    separated markup into components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="container">
+			<Header />
+
+			<main id="site-content"></main>
+
+			<footer id="site-footer">
+				<p>@PetMyPet</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
